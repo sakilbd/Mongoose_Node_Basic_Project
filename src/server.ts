c1120e60db1817516d270dc254321a7d3931e1f2
unknown <mdsakiluzzaman@gmail.com>
2023-11-18 19:56:22 +0600

    some updates

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ async function main() {
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`app listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
