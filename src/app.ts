880c9fb97bae73a2e23518d3132709581600c639
sakilbd <mdsakiluzzaman@gmail.com>
2023-11-19 11:33:00 +0600

    student create ,param single student get done

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRouters } from './app/modules/student/student.route';
 const app = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+
+
+//api/v1/students/create-student
+//application routers
+app.use('/api/v1/students', StudentRouters);
+
+
+
+const getAController = (req: Request, res: Response) => {
   let a = 10;
 
   res.send(a);
-});
+}
+app.get('/', getAController);
 
 export default app;
